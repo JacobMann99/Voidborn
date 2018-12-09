9fed03c14190756897fa2939b5c4f9d1ab454331
cjz2501 <cjz2501@gmail.com>
2018-12-08 22:03:20 -0600

    Added the InputHandler to take in arguments and returns movement.

--- src/mann/game/master/InputHandler.java
+++ src/mann/game/master/InputHandler.java
@@ -9,13 +9,17 @@ import mann.game.graphics.GraphicsEngine;
  * Accepts Key Events from the user and raises flags so the level can tell when the user wants to do something.
  * @author Jacob Mann
  */
-public class InputHandler implements KeyListener {
 
+public class InputHandler implements KeyListener {
+	private Boolean movementUp;
+	private Boolean movementDown;
+	private Boolean movementLeft;
+	private Boolean movementRight;
 	/**
 	 * Adds the KeyListener to the frame upon initialization
 	 * @param graphics
 	 *            GraphicsEngine class passed in from the VoidbornApplication main class
-	 */
+	 VK_LEFT, VK_DOWN, VK_RIGHT, VK_UP*/
 	public InputHandler(GraphicsEngine graphics) {
 		graphics.getFrame().addKeyListener(this);
 	}
@@ -24,7 +28,12 @@ public class InputHandler implements KeyListener {
 	public void keyPressed(KeyEvent arg0) {
 		// Prints KEY EVENT to console when any key is pressed.
 		System.out.println("KEY EVENT");
-	}
+		movementUp = getMovementUp(arg0);
+		movementDown = getMovementDown(arg0);
+		movementLeft = getMovementLeft(arg0);
+		movementRight = getMovementRight(arg0);
+		
+		}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
@@ -35,5 +44,36 @@ public class InputHandler implements KeyListener {
 	public void keyTyped(KeyEvent arg0) {
 		// Probably will be unnecessary, but this method's presence is required for every KeyListener interface
 	}
-
+	@SuppressWarnings("unlikely-arg-type")
+	public Boolean getMovementUp(KeyEvent arg0) {
+		if(arg0.equals(KeyEvent.VK_UP)||(arg0.equals(KeyEvent.VK_W))) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	@SuppressWarnings("unlikely-arg-type")
+	public Boolean getMovementDown(KeyEvent arg0) {
+		if(arg0.equals(KeyEvent.VK_DOWN)||(arg0.equals(KeyEvent.VK_S))) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	@SuppressWarnings("unlikely-arg-type")
+	public Boolean getMovementLeft(KeyEvent arg0) {
+		if(arg0.equals(KeyEvent.VK_LEFT)||(arg0.equals(KeyEvent.VK_A))) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	@SuppressWarnings("unlikely-arg-type")
+	public Boolean getMovementRight(KeyEvent arg0) {
+		if(arg0.equals(KeyEvent.VK_RIGHT)||arg0.equals(KeyEvent.VK_D)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
